55cc027740d131dde131a4601b9864d9f483a2e5
Yoshi <hiroluk@gmail.com>
2015-11-09 12:47:28 -0800

    final edits to index page

--- app/assets/javascripts/components/tag_gif_index_item.js.jsx
+++ app/assets/javascripts/components/tag_gif_index_item.js.jsx
@@ -1,14 +1,12 @@
 var TagGifIndexItem = React.createClass({
   render: function(){
       return (
-        <div>
-          <div className="gif-index-item-tag">
-            {
-              this.props.tags.map(function(tag){
-                return "#" + tag.tag_title + " ";
-              })
-            }
-          </div>
+        <div className="gif-index-item-tag">
+          {
+            this.props.tags.map(function(tag){
+              return "#" + tag.tag_title + " ";
+            })
+          }
         </div>
       )
   }
